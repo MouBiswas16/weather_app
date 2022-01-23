1eb408e4988bf4b0af2d65cc8867748addf6c02a
Mou Biswas <qtmeaowmou16@gmail.com>
2022-01-23 15:25:44 +0600

    day 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:apps/screens/home_page.dart';
+import 'package:apps/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,8 +2,8 @@
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
