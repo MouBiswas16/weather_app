2ea0f23400cfcb0fbf2f308ea5520d8a2dbe9a4a
Mou Biswas <qtmeaowmou16@gmail.com>
2022-01-28 00:35:58 +0600

    day 3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, non_constant_identifier_names
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, non_constant_identifier_names, sized_box_for_whitespace
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -16,111 +16,184 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffF1F0FA),
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(top: defultPaddin),
-          child: Column(
-            children: [
-              Container(
-                child: Column(
-                  children: [
-                    Text(
-                      "Pasuruan",
-                      style: TextStyle(
-                        color: textColor,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 24,
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    "Pasuruan",
+                    style: TextStyle(
+                      color: textColor,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    "17.45 PM",
+                    style: TextStyle(
+                      color: textColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(height: defultPaddin * 2),
+                  CarouselSlider(
+                    options: CarouselOptions(height: 350),
+                    items: [1, 2, 3, 4].map((i) {
+                      return Builder(builder: (BuildContext context) {
+                        return DailyWeatherCard();
+                      });
+                    }).toList(),
+                  ),
+                  // Stack(
+                  //   alignment: Alignment.bottomCenter,
+                  //   children: [
+                  //     Positioned(
+                  //       bottom: 517,
+                  //       child: SizedBox(
+                  //         height: 260,
+                  //         width: double.infinity,
+                  //         child: Container(
+                  //           decoration: BoxDecoration(
+                  //             color: Color(0xffD4D1F0),
+                  //           ),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
+            ),
+            Container(
+              height: 122,
+              width: 315,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      WeatherDetails(
+                        imageName: "carbon_humidity",
+                        value: "75%",
+                        label: "Humidity",
                       ),
-                    ),
-                    Text(
-                      "17.45 PM",
-                      style: TextStyle(
-                        color: textColor,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 12,
+                      SizedBox(width: defultPaddin),
+                      WeatherDetails(
+                        imageName: "tabler_wind",
+                        value: "8 km/h",
+                        label: "Wind",
                       ),
-                    ),
-                    SizedBox(height: defultPaddin * 2),
-                    CarouselSlider(
-                      options: CarouselOptions(height: 350),
-                      items: [1, 2, 3, 4].map((i) {
-                        return Builder(builder: (BuildContext context) {
-                          return DailyWeatherCard();
-                        });
-                      }).toList(),
-                    ),
-                    SizedBox(
-                      height: 112,
-                    ),
-                    Stack(
-                      alignment: Alignment.bottomCenter,
+                      SizedBox(width: defultPaddin),
+                      WeatherDetails(
+                        imageName: "ion_speedometer",
+                        value: "1011",
+                        label: "Air Pressure",
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: defultPaddin, vertical: defultPaddin),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Positioned(
-                          bottom: 517,
-                          child: SizedBox(
-                            height: 260,
-                            width: double.infinity,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Color(0xffD4D1F0),
+                        Text(
+                          "Today",
+                          style: TextStyle(
+                              color: textColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w900),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Next 7 Days",
+                              style: TextStyle(
+                                color: textColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w900,
                               ),
                             ),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 434,
-                          child: Container(
-                            height: 122,
-                            width: 315,
-                            // margin: EdgeInsets.symmetric(horizontal: 20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: textColor,
                             ),
-                            child: Column(
-                              // mainAxisAlignment: MainAxisAlignment.center,
-                              // crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    WeatherDetails(
-                                      imageName: "carbon_humidity",
-                                      value: "75%",
-                                      label: "Humidity",
-                                    ),
-                                    SizedBox(width: defultPaddin),
-                                    WeatherDetails(
-                                      imageName: "tabler_wind",
-                                      value: "8 km/h",
-                                      label: "Wind",
-                                    ),
-                                    SizedBox(width: defultPaddin),
-                                    WeatherDetails(
-                                      imageName: "ion_speedometer",
-                                      value: "1011",
-                                      label: "Air Pressure",
-                                    ),
-                                    SizedBox(width: defultPaddin),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    height: 132,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: 7,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return HourlyWeatherCard();
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class HourlyWeatherCard extends StatelessWidget {
+  const HourlyWeatherCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 92,
+      height: 132,
+      margin: EdgeInsets.symmetric(horizontal: defultPaddin),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff594DB5),
+            Color(0xff928ACE),
+            Color(0xff594DB5),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "06:00 AM",
+            style: TextStyle(
+              color: textLightColor,
+              fontSize: 11,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SvgPicture.asset("assets/images/Moon_cloud_fast_wind.svg"),
+          Text(
+            "24°",
+            style: TextStyle(
+              color: textLightColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+        ],
       ),
     );
   }
